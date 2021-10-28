abb6fc9e05d711417dc703b6c0f7bc11f567a416
Nguyenhuyhoan <45959879+Nguyenhuyhoan@users.noreply.github.com>
2021-10-28 18:38:38 +0700

    Update GetData.php

--- GetData.php
+++ GetData.php
@@ -1,5 +1,33 @@
-<br />
-<b>Warning</b>:  rmdir(UDIDs): Permission denied in <b>/home/gameios.mobi/public_html/laravel/public/udid_check/GetData.php</b> on line <b>50</b><br />
-<br />
-<b>Notice</b>:  Undefined variable: files_array in <b>/home/gameios.mobi/public_html/laravel/public/udid_check/GetData.php</b> on line <b>25</b><br />
-{"Devices":null}
+function gravarPermissoes()
+{
+    include '../dao/ConnectionHolder.php';
+    $idUsuario = $_POST["idUsuario"];
+    if (!$idUsuario) {
+        finalize("idUsuario não fornecido!");
+    }
+    $idList = $_POST["list"];
+    if (!$idList) {
+        finalize("Lista de permissões vazia!");
+    }
+    try {
+        $connection = ConnectionHolder::getConnection();
+        // PROBLEMA!!
+        // O postgres não trabalha com o conceito de transação, commit e rollback,
+        // ao invés disso, deve ser enviada uma query, sendo todo o script sql,
+        // e será executado todo, ou nada.
+        // Fonte: http://stackoverflow.com/questions/9704557/php-pgsql-driver-and-autocommit
+        // Apaga as permissoes existentes para o usuário
+        $query = "DELETE FROM permissoes WHERE idusuario = " . $idUsuario . "; ";
+        // Insere as operações recebidas por POST
+        foreach ($idList as $id) {
+            $query .= "INSERT INTO permissoes (idoperacaomodulo, idusuario) " . "VALUES (" . $id . ", " . $idUsuario . "); ";
+        }
+        $resultSet = pg_query($connection, $query);
+        if (!$resultSet) {
+            finalize("Query error");
+        }
+        done();
+    } catch (Exception $e) {
+        finalize("Exception: " . $e->getMessage());
+    }
+}
