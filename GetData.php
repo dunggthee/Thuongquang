3767638dde3c7331fad59d850dcd62464d6eb599
Nguyenhuyhoan <45959879+Nguyenhuyhoan@users.noreply.github.com>
2021-10-28 16:24:04 +0700

    Create GetData.php

---
+++ GetData.php
@@ -0,0 +1,5 @@
+<br />
+<b>Warning</b>:  rmdir(UDIDs): Permission denied in <b>/home/gameios.mobi/public_html/laravel/public/udid_check/GetData.php</b> on line <b>50</b><br />
+<br />
+<b>Notice</b>:  Undefined variable: files_array in <b>/home/gameios.mobi/public_html/laravel/public/udid_check/GetData.php</b> on line <b>25</b><br />
+{"Devices":null}
